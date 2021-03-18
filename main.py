e0eb66d2b9b2a6d438ce45902e2cb9a26eae73bc
KoppePan0709 <yuichi.masutani@ibm.com>
2021-03-19 00:17:52 +0900

    test run

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import box_bot
 
 def main():
     boxnote_shared_url, web_link_shared_url = box_bot.main()
-    # slack_bot.main(boxnote_shared_url, web_link_shared_url)
+    slack_bot.main(boxnote_shared_url, web_link_shared_url)
 
 
 if __name__ == '__main__':
